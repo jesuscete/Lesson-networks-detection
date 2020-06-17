6f175c8fb4e9c8169fc9e66cc23507909bc2348b
Jesus <jesuscete1994@gmail.com>
2020-06-17 18:47:57 +0200

    Preparate Transofrm function
    First step of module for organizate data anda transform images

--- Busqueda.py
+++ Busqueda.py
@@ -1,8 +1,14 @@
 import glob, os
 my_path = os.path.abspath(os.path.dirname(__file__))
-movedPath = os.path.join(my_path, "../Datos/Atlas/Site3/")
+movedPath = os.path.join(my_path, "../Datos/DatosOriginales/")
 os.chdir(movedPath)
-imagenes = glob.glob("**/**/*.nii.gz")
-for i in imagenes:
-    if("LesionSmooth" in i):
-        print(i)
+subjects = glob.glob("**")
+for i in subjects:
+    #print(i)
+    cadena = str(i+"/t01")
+    os.chdir(movedPath+cadena)
+    brainImage = glob.glob("*.gz")
+    for j in brainImage:
+        print(j)
+    #if("LesionSmooth" in i):
+    #    print(i)
