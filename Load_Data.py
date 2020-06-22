99b443a53236f9df194afa475650638ca01a4925
Jesus <jesuscete1994@gmail.com>
2020-06-22 11:29:59 +0200

    Load_Coord_Exampl
    Para hacer las pruebas pertinentes se crea una función que carga y devuelve un sujeto de prueba, junto a su matriz imagen y las coordenadas para el timeserie

--- Load_Data.py
+++ Load_Data.py
@@ -2,10 +2,11 @@ import glob, os
 import ants
 from pathlib import Path
 import numpy as np
+import nibabel as nib
 def Load_Timeseries():
     mod_path = Path(__file__).parent
     #A través de una ruta relativa buscamos la carpeta de almacenamiento de datos.
-    pathOfData =  str((mod_path / str("../Datos/timeseries_N10")))
+    pathOfData =  str((mod_path / str("../Datos/timeseries_N10")).resolve())
     os.chdir(pathOfData)
     #Partiendo de la estructura: ../DatosOriginales/sujeto/t01/Imagenes
     #Cargamos todos los sujetos
@@ -18,3 +19,15 @@ def Load_Timeseries():
     #print("Matriz coordenadas:\n", coordenadas)
     #print("timeseries:\n",timeseries)
     return coordenadas,timeseries
+
+def Load_Coord_Example():
+    cwd = Path.cwd()
+    mod_path = Path(__file__).parent
+    #A través de una ruta relativa buscamos la carpeta de almacenamiento de datos.
+    pathOfData =  str((mod_path / '../Datos/DataWorking/031768').resolve())
+    os.chdir(pathOfData)
+    #Cargamos con numpy, la matriz de dentro del fichero.
+    ImageExample = nib.load(pathOfData+"/031768_lesson[1].nii.gz")
+    mascaraMatriz = ImageExample.get_fdata()
+    x,y,z = np.where(mascaraMatriz != 0)
+    return ImageExample,x,y,z
--- SeedBasedAnalysis.py
+++ SeedBasedAnalysis.py
@@ -1,5 +1,10 @@
 import numpy as np
 import Load_Data as ld
-coord, timeseries = ld.Load_Timeseries()
-coord = np.int_(coord)
-seed_timeseries = np.mean(timeseries[coord],axis=0)
+import ants
+import nibabel as nib
+#coord, timeseries = ld.Load_Timeseries()
+#coord = np.int_(coord)
+mascaraMatrix, x,y,z = ld.Load_Data_Example()
+
+
+#seed_timeseries = np.mean(timeseries[coord],axis=0)
